d7d7a2dfbe0b74a4f239c99b93953d4f3204db5e
FelixvL <digitaleservice@gmail.com>
2020-03-01 11:53:28 +0100

    startsituatie .gitignore aangemaakt

---
+++ src/main/java/nl/colfield/bankappbe/BankappbeApplication.java
@@ -0,0 +1,13 @@
+package nl.colfield.bankappbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankappbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankappbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/colfield/bankappbe/BankappbeApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.colfield.bankappbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankappbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
