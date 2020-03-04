5eabca5fdaea6aa8c40e093c0c917dc3e24e2799
Kevin <kevindamen_1@hotmail.com>
2020-03-04 11:33:24 +0100

    added feature display all customers

---
+++ src/main/java/nl/colfield/bankappbe/api/CustomerEndpoint.java
@@ -0,0 +1,48 @@
+package nl.colfield.bankappbe.api;
+
+import nl.colfield.bankappbe.controller.CustomerService;
+import nl.colfield.bankappbe.domain.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class CustomerEndpoint {
+	
+	@Autowired
+	private CustomerService customerService;
+
+	@GetMapping("trial")
+	public String returnString() {
+		return "De BankApp is in de Lucht, met de tweede testcommit";
+	}
+	@GetMapping("emptycustomer")
+	public Customer returnCustomer() {
+		return new Customer();
+	}
+	@GetMapping("customer/newfake")
+	public String newCustomerFake() {
+		customerService.newCustomerFake();
+		return "New customer added";
+	}
+	@PostMapping("customer/new")
+	public String trailPost(@RequestBody Customer customer){
+		System.out.println(customer.getFirstName());
+		customerService.addNewCostumer(customer);
+		return "gelukt";
+	}
+
+	@DeleteMapping("customer/{id}/delete")
+	public String trailDelete(@PathVariable long id){
+		customerService.deleteCostumer(id);
+
+		return "Klant verwijderd";
+
+	}
+
+	@GetMapping("customer/displayAll")
+	public Iterable<Customer> getAllCustomers(){
+		return customerService.getAllCustomers();
+	}
+
+
+}
--- src/main/java/nl/colfield/bankappbe/api/TrialEndpoint.java
+++ src/main/java/nl/colfield/bankappbe/api/TrialEndpoint.java
@@ -1,48 +1,48 @@
-package nl.colfield.bankappbe.api;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import nl.colfield.bankappbe.controller.CustomerService;
-import nl.colfield.bankappbe.domain.Customer;
-
-@RestController
-public class TrialEndpoint {
-	
-	@Autowired
-	private CustomerService customerService;
-
-	@GetMapping("trial")
-	public String returnString() {
-		return "De BankApp is in de Lucht, met de tweede testcommit";
-	}
-	@GetMapping("emptycustomer")
-	public Customer returnCustomer() {
-		return new Customer();
-	}
-	@GetMapping("customer/all")
-	public Iterable<Customer> getAllCustomers(){
-		return customerService.getAllCustomers();
-	}
-	@GetMapping("customer/newfake")
-	public String newCustomerFake() {
-		customerService.newCustomerFake();
-		return "New customer added";
-	}
-	@PostMapping("customer/new")
-	public String trailPost(@RequestBody Customer customer){
-		System.out.println(customer.getFirstName());
-		customerService.addNewCostumer(customer);
-		return "gelukt";
-	}
-
-	@DeleteMapping("customer/{id}/delete")
-	public String trailDelete(@PathVariable long id){
-		customerService.deleteCostumer(id);
-
-		return "Klant verwijderd";
-
-	}
-
-
-}
+//package nl.colfield.bankappbe.api;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//import nl.colfield.bankappbe.controller.CustomerService;
+//import nl.colfield.bankappbe.domain.Customer;
+//
+//@RestController
+//public class TrialEndpoint {
+//
+//	@Autowired
+//	private CustomerService customerService;
+//
+//	@GetMapping("trial")
+//	public String returnString() {
+//		return "De BankApp is in de Lucht, met de tweede testcommit";
+//	}
+//	@GetMapping("emptycustomer")
+//	public Customer returnCustomer() {
+//		return new Customer();
+//	}
+//	@GetMapping("customer/all")
+//	public Iterable<Customer> getAllCustomers(){
+//		return customerService.getAllCustomers();
+//	}
+//	@GetMapping("customer/newfake")
+//	public String newCustomerFake() {
+//		customerService.newCustomerFake();
+//		return "New customer added";
+//	}
+//	@PostMapping("customer/new")
+//	public String trailPost(@RequestBody Customer customer){
+//		System.out.println(customer.getFirstName());
+//		customerService.addNewCostumer(customer);
+//		return "gelukt";
+//	}
+//
+//	@DeleteMapping("customer/{id}/delete")
+//	public String trailDelete(@PathVariable long id){
+//		customerService.deleteCostumer(id);
+//
+//		return "Klant verwijderd";
+//
+//	}
+//
+//
+//}
