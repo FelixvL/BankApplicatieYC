13fa4aed8cad9abaf8bc93e95190a1c86e6c0271
Kevin <kevindamen_1@hotmail.com>
2020-03-04 11:48:27 +0100

    Remove TrailEndpoint and removed example features

--- src/main/java/nl/colfield/bankappbe/api/CustomerEndpoint.java
+++ src/main/java/nl/colfield/bankappbe/api/CustomerEndpoint.java
@@ -11,19 +11,6 @@ public class CustomerEndpoint {
 	@Autowired
 	private CustomerService customerService;
 
-	@GetMapping("trial")
-	public String returnString() {
-		return "De BankApp is in de Lucht, met de tweede testcommit";
-	}
-	@GetMapping("emptycustomer")
-	public Customer returnCustomer() {
-		return new Customer();
-	}
-	@GetMapping("customer/newfake")
-	public String newCustomerFake() {
-		customerService.newCustomerFake();
-		return "New customer added";
-	}
 	@PostMapping("customer/new")
 	public String trailPost(@RequestBody Customer customer){
 		System.out.println(customer.getFirstName());
@@ -34,7 +21,6 @@ public class CustomerEndpoint {
 	@DeleteMapping("customer/delete/{id}")
 	public String trailDelete(@PathVariable long id){
 		customerService.deleteCostumer(id);
-
 		return "Klant verwijderd";
 
 	}
