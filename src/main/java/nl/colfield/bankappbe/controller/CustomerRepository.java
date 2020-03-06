ebf361f5dda1a48169435bd960bdde40cebc5efd
Kevin <kevindamen_1@hotmail.com>
2020-03-06 10:19:46 +0100

    added-search-function

--- src/main/java/nl/colfield/bankappbe/controller/CustomerRepository.java
+++ src/main/java/nl/colfield/bankappbe/controller/CustomerRepository.java
@@ -1,11 +1,17 @@
 package nl.colfield.bankappbe.controller;
 
+import nl.colfield.bankappbe.domain.Account;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
 
 import nl.colfield.bankappbe.domain.Customer;
 
+import java.util.List;
+
 @Component
 public interface CustomerRepository extends CrudRepository<Customer,Long>{
 
+    List<Customer> findByFirstNameLikeOrLastNameLike (String firstname, String lastname);
+
 }
+
